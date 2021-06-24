3188214218b77c92e231b249b82d4bc77b9e309b
Dima <dima.tolmachev@gmail.com>
2021-06-24 12:31:27 +0400

    update

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const url = require('url');
 const path = require('path');
 const port = 3333;
 
 const app = express();
 const admin = express();
+
+app.get('/hello', (req, res) => {
+    let urlRequest = url.parse(req.url, true);
+    res.send(urlRequest.query.key);
+});
 
 app.use(express.static('site'));
 
