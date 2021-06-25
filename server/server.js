828f712f155990ae4c5a5925487c3fbcef753269
Dima <dima.tolmachev@gmail.com>
2021-06-25 13:57:59 +0400

    update

--- server/server.js
+++ server/server.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const url = require('url');
 const path = require('path');
+const fs = require('fs');
 const port = 3333;
 
 const app = express();
 const admin = express();
 
-app.get('/hello', (req, res) => {
+app.get('/authByKey', (req, res) => {
     let urlRequest = url.parse(req.url, true);
-    res.send(urlRequest.query.key);
+    var key = urlRequest.query.key;
+
+    fs.readFile(`../keys/${key}.json`, 'utf8', (err, jsonString) => {
+        if (err) {
+            res.send("The system can not find this key");
+            return;
+        } else {
+            res.send("OK");
+        }
+    });    
 });
 
-app.use(express.static('site'));
+app.use(express.static('../site'));
 
 
 app.listen(port, () => {
